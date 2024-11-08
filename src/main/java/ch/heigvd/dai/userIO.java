e5c16100adb673478a45934c4a3a9638c9d7d75b
thomasstaheli <thomas_s@bluewin.ch>
2024-11-08 11:11:38 +0100

    Deplacement des fichiers + correction code

---
+++ src/main/java/ch/heigvd/dai/userIO.java
@@ -0,0 +1,26 @@
+package ch.heigvd.dai;
+
+import java.util.Scanner;
+
+public class userIO {
+
+  static public int getIntInput(int minValue, int maxValue)
+  {
+    int input=0; //must initialize to avoid error (var might not have been initialized)
+    Scanner scanner = new Scanner(System.in); //input code source : w3s Java input
+    boolean validInput = false;
+    do{
+      System.out.println("Veuillez choisir un numéro de colonne entre "+minValue+" et "+maxValue);
+      if(scanner.hasNextInt()) {
+        input = scanner.nextInt();
+        validInput = input >= minValue && input <= maxValue;
+      }
+
+
+    }
+    while(!validInput); //had to create a var because I can't put the hasNextInt here,
+    // it cannot scan for the first time
+    return input;
+  }
+
+}
