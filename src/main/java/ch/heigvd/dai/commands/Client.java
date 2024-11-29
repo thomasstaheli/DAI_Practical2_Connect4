1b51e88e9af5b3be30dfed6e2982582960da0b48
Thiru-sama <thirusan.rajadurai@gmail.com>
2024-11-29 11:38:32 +0100

    added commands for client and server

--- src/main/java/ch/heigvd/dai/commands/Client.java
+++ src/main/java/ch/heigvd/dai/commands/Client.java
@@ -1,4 +1,33 @@
 package ch.heigvd.dai.commands;
 
-public class Client {
+import java.util.concurrent.Callable;
+
+import ch.heigvd.dai.Display;
+import ch.heigvd.dai.GameBoard;
+import ch.heigvd.dai.network.TcpClient;
+import ch.heigvd.dai.userIO;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "client", description = "Connect as a client, so you will be a player")
+public class Client implements Callable<Integer>{
+
+    @CommandLine.ParentCommand protected Root parent;
+    @CommandLine.Option(
+            names = {"--ip"},
+            description = "The ip address of the server where the game is hosted\n",
+            required = true
+    )
+
+    protected String ip;
+
+    @Override
+    public Integer call() {
+
+        System.out.println("Connecting to the server with ip " + ip + "\n");
+        TcpClient client = new TcpClient(ip);
+        client.run();
+
+        return 0;
+    }
 }
--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -1,4 +1,16 @@
 package ch.heigvd.dai.commands;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(
+        description = "A small CLI to apply visual effects on bmp image files.",
+        version = "1.0.0",
+        subcommands = {
+                Client.class,
+                Server.class
+        },
+        scope = CommandLine.ScopeType.INHERIT,
+        mixinStandardHelpOptions = true)
 public class Root {
+
 }
